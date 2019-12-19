de3a4c32a7e53ad65931d1adfc72142573508fff
Alfredo <alfredodomin91@gmail.com>
2019-12-19 15:09:06 -0600

    making object with prompt function-progress not done

--- app.js
+++ app.js
@@ -1,38 +1,93 @@
 
 const fs = require("fs"); // loading fs library
-const inquirer = require("inquirer"); // library to promp user
+const inquirer = require ('inquirer') // library to prompt user
 
-const Engineer = require("./lib/Employee.js")
-const Manager = require("./lib/Manager.js")
-const Engineer = require("./lib/Engineer.js")
-const Intern = require("./lib/Intern,js")
+const Employee = require("./lib/Employee"); //  class templates
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
-const generateHtml = require("./templates/main.js");
 
-promptUser(){
+var dataObj = {
+    data: [],
+}
+promptUser()
+// buildEmployeeTemplate();
+
+function promptUser(){
     return inquirer.prompt([
         {
-        message: 'What is your name',
-        name:'name'
+          type: "input",
+          message: "Input the employee's name please.",
+          name: "name",
         },
         {
-        message:'What is your company ID?',
-        name:'id'  
+            type: "input",
+            message: "Also, input his employee ID.",
+            name: "id",
         },
         {
-        message:'What is your title (Manager, Engineer or Intern)?',
-        name:'title'  
+          type: "list",
+          message: "Great! Choose the appropriate title for them.",
+          name: "title",
+          choices: ["Manager", "Engineer", "Intern"],
+        },
+        {
+          type: "input",
+          message: "What is the employee's email?",
+          name: "email"
         }
-    ]).then(answers => {
-        // console.log(this); //operator this refer to newHomework Object
-        this.githubUserName = answers.githubUserName
-        this.color = answers.color
-        // console.log(this);
-        this.makeApiRequest();
+    ]).then (promptRes => {
+        var temp_EmployeeObj = new Employee (promptRes.name,promptRes.id, promptRes.email); // creating new object from constructor funciton 
+        console.log (temp_EmployeeObj);
 
+        var title = promptRes.title; // use 'title' & conditional to prompt user with correct Qs
+        if (title === "Manager"){
+            promptManagerQs(
+                temp_EmployeeObj.name, // setting Obj properties
+                temp_EmployeeObj.id, 
+                temp_EmployeeObj.email
+            )
+        }
+        else if (title === "Engineer"){
+            promptEngineerQs(
+                temp_EmployeeObj.name, 
+                temp_EmployeeObj.id, 
+                temp_EmployeeObj.email
+            )
+        }
+        else if (title === "Intern"){
+            promptInternQs(
+                temp_EmployeeObj.name, 
+                temp_EmployeeObj.id, 
+                temp_EmployeeObj.email
+            )
+        }
+        
     })
 }
 
+function buildEmployeeTemplate() {
 
+}
 
+function promptManagerQs(){
+    return inquirer.prompt({
+        type: "input",
+        message: "question?",
+        name: "ManagerQs"
+      }).then(answer => {
+       console.log(answer)
+      })
+}
 
+function promptEngineerQs(){
+
+}
+
+function promptInternQs() {
+
+}
+function createHTML(){
+
+}
