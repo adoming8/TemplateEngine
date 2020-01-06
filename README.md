04943b02782790a9ff28add8626d6a890806ad8e
Alfredo <alfredodomin91@gmail.com>
2020-01-05 21:44:17 -0600

    second round of update, completing App

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# Unit 10 OOP Homework: Template Engine - Employee Summary
+# Team Profile Generator - Command line App 
+
+    This is a node.js command line application focused in helping manage a group of software developers from a management level. Initialyl, the app promps the user with a variety of questions that will shape/creat an html page with the employees' personal infomation.
++ [Team Profile Generator](https://github.com/adoming8/Unit10_TemplateEngine.git "Team Profile Generator") 
 
 
-TBD
+## Topics covered in this activity- Object Oriented Programing & Test Driven-Development
 
-## Instructions
++ Unit 10 OOP HW: Initial introduction of constructor functions and prototype chain.  Constructors are special functions that are extremely useful in creating objects of similar types. Prototypes are JavaScript’s built in system, allowing objects to inherit features from other objects.
++ 
+    
+
+## General instructions: How to start using the application
++ Clone the GitHub Repository to local machine
++ Install Dependencies - 'npm install'
++ Run the Application - 'npm start'
++ Running the Tests - 'npm test'
+
+## Software's used to the build App
++ node.js
++ styling framework - Bootstrap
 
--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 const fs = require("fs"); // loading fs library
 const inquirer = require ('inquirer') // library to prompt user
 
+
 const Employee = require("./lib/Employee"); //  class templates
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
 
-promptUser();
 
+promptUser();
 buildEmployeeTemplate();
 
 function promptUser(){
@@ -45,6 +46,8 @@ function promptUser(){
                 temp_EmployeeObj.id, 
                 temp_EmployeeObj.email
             )
+            console.log(temp_EmployeeObj.name)
+
         }
         else if (title === "Engineer"){
             promptEngineerQs(
@@ -64,26 +67,46 @@ function promptUser(){
     
 }
 
-function promptManagerQs(id,name,email){
+function promptManagerQs(name,id, email){
     return inquirer.prompt({
         type: "input",
         message: "What is your office number?",
         name: "addiPro_2NewObj",
       }).then(answer => {
-    //    console.log(answer)
-    var newManager = new Manager(name, id, email,answer.addiPro_2NewObj)
+       console.log(answer)
+    var newManager = new Manager(name, id, email,answer.addiPro_2NewObj);
         console.log(newManager)
 
-    // buildEmployeeTemplate();
+    buildEmployeeTemplate();
       })
 }
 
-function promptEngineerQs(){
+function promptEngineerQs(name,id, email){
+    return inquirer.prompt({
+        type: "input",
+        message: "What is your GitHub Username?",
+        name: "addiPro_2NewObj",
+      }).then(answer => {
+    //    console.log(answer)
+    var newEngineer = new Engineer(name, id, email,answer.addiPro_2NewObj)
+        console.log(newEngineer)
 
+    buildEmployeeTemplate();
+      })
 }
 
-function promptInternQs() {
+function promptInternQs(name,id, email) {
+    return inquirer.prompt({
+        type: "input",
+        message: "What school are you enrolled in?",
+        name: "addiPro_2NewObj",
+      }).then(answer => {
+    //    console.log(answer)
+    var newIntern = new Intern (name, id, email,answer.addiPro_2NewObj)
+        console.log(newIntern)
 
+    buildEmployeeTemplate();
+      })
 }
 
 function buildEmployeeTemplate() {
@@ -94,3 +117,4 @@ function buildEmployeeTemplate() {
 function createHTML(){
 
 }
+
